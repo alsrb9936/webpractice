f788776081c4c8b68a392b22870c137718eb38fa
MinGyu <ghfn123@gmail.com>
2021-01-20 12:00:21 +0000

    CBV로 블로그 포스트 목록 페이지 만들기

--- python-project/mingyu/blog/views.py
+++ python-project/mingyu/blog/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' :posts,
-        }
-    )
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    ordering = '-pk'
+
+# def index(request):
+#    posts = Post.objects.all().order_by('-pk')
+#    
+#    return render(
+#        request,
+#        'blog/index.html',
+#        {
+#            'posts' :posts,
+#        }
+#    )
