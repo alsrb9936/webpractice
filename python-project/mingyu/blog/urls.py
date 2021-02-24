e8ee40e2d1dee72eaaa173683ace9612cef0e0cd
MinGyu <ghfn123@gmail.com>
2021-02-24 10:44:41 +0000

    태그 페이지 만들기

--- python-project/mingyu/blog/urls.py
+++ python-project/mingyu/blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('<int:pk>/',views.PostDetail.as_view()),
     path('',views.PostList.as_view()),
-    path('category/<str:slug>/',views.category_page),
+    path('category/<str:slug>/', views.category_page),
+    path('tag/<str:slug>/', views.tag_page),
 #    path('',views.index),
     
 ]
