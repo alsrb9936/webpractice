2bab33aeaf45b4591e44af0eacb14324504b53b7
MinGyu <ghfn123@gmail.com>
2021-02-28 12:05:03 +0000

    포스트 작성 페이지 접근 권한 설정

--- python-project/mingyu/blog/tests.py
+++ python-project/mingyu/blog/tests.py
@@ -9,6 +9,8 @@ class TestView(TestCase):
         self.client = Client()
         self.user_trump = User.objects.create_user(username='trump',password='somepassword')
         self.user_obama = User.objects.create_user(username='obama',password='somepassword')
+        self.user_obama.is_staff = True
+        self.user_obama.save()
         
         self.category_programming = Category.objects.create(name='programming', slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
@@ -172,11 +174,16 @@ class TestView(TestCase):
         response = self.client.get('/blog/create_post/')
         self.assertNotEqual(response.status_code, 200)
         
-        #로그인을 한다
+        #스태프가 아닌 유저가 로그인을 한다
         self.client.login(username='trump', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
         
+        #스태프인 유저가 로그인을 한다.
+        self.client.login(username='obama', password='somepassword')
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
+        
         soup = BeautifulSoup(response.content, 'html.parser')
         
         self.assertEqual('Create Post - Blog', soup.title.text)
@@ -192,5 +199,5 @@ class TestView(TestCase):
         )
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
-        self.assertEqual(last_post.author.username, "trump")
+        self.assertEqual(last_post.author.username, "obama")
         
